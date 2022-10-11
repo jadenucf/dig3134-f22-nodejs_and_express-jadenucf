ca9b4c6789a1de0becb09ad67275c7ab1ffe5b09
Jaden Lewis-Chambers <you@example.com>
2022-10-11 14:16:31 -0400

    testfiles must remain in the repository

--- 5/ex5/cat.js
+++ 5/ex5/cat.js
@@ -9,11 +9,10 @@ console.log(text)
 
 // 2. cp
 const data = ""
-fs.writeFileSync("./testDir/TestFile2.txt", data, function (err) {
+fs.writeFileSync(path.dirname + "./testDir/TestFile2.txt", data, function (err) {
   if (err) {
     return console.log(err)
   }
   console.log("goodnight, punpun!")
 })
-
 fs.copyFileSync("./testDir/TestFile.txt", "./testDir/TestFile2.txt")
