9260e33f6d51b3be290b457e3c537173f921d2fc
Jaden Lewis-Chambers <you@example.com>
2022-10-11 13:41:51 -0400

    syncing files

--- 5/ex5/cat.js
+++ 5/ex5/cat.js
@@ -5,8 +5,7 @@ import path from "path"
 
 // 1. cat
 const text = fs.readFileSync("./testDir/TestFile.txt", "UTF-8")
-//console.log(text)
+console.log(text)
 
 // 2. cp
-const copy = fs.copyFileSync("./testDir/TestFile.txt", "./testDir/TestFile2.txt")
-console.log(copy)
+fs.copyFileSync(text, "./testDir/TestFile2.txt")
