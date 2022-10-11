8df69121ea4943a162f8199d20763f303fa0c1d4
Jaden Lewis-Chambers <you@example.com>
2022-10-11 22:36:50 +0000

    Oct 11, 2022, 6:36 PM

--- 5/ex5/cp.js
+++ 5/ex5/cp.js
@@ -1,17 +1,17 @@
+
+// 2. cp
 import fs from "fs"
 import path from "path"
 
-// 2. cp
 const data = ""
 const file2 = "./testDir/TestFile2.txt"
-
 fs.writeFileSync(file2, data, function (err) {
   if (err) {
     return console.log(err)
   }
-  console.log("hello")
+  console.log("the file has been created")
 })
 
-const copy = fs.copyFileSync(path.dirname(file2), "./testDir/TestFile2.txt")
+const copy = fs.copyFileSync("./testDir/Testfile.txt", "./testDir/TestFile2.txt")
 const out = fs.readFileSync(copy, "UTF-8")
 console.log(out)
---
+++ 5/ex5/echo.js
@@ -0,0 +1,4 @@
+import { testString } from "../ex5.test"
+
+
+process.stdout.write(testString)
