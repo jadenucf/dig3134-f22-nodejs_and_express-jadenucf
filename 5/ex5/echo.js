14116777649d49521ba3625e8978f8fa0516c85b
Jaden Lewis-Chambers <you@example.com>
2022-10-12 15:01:48 +0000

    Oct 12, 2022, 11:01 AM

--- 5/ex5/echo.js
+++ 5/ex5/echo.js
@@ -2,6 +2,12 @@
 import { gn } from "./cp.js"
 import path from "path"
 
+
+import { fileURLToPath } from "url"
+import { dirname } from "path"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 path.resolve(path.dirname("./cp.js"))
 
 console.log(gn)
