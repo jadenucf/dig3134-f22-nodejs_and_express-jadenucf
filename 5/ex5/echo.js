f827ecd00ce78b7b5db9aeda74ed20daf92b9e66
Jaden Lewis-Chambers <you@example.com>
2022-10-12 06:12:14 +0000

    Oct 12, 2022, 2:12 AM

--- 5/ex5/echo.js
+++ 5/ex5/echo.js
@@ -1,5 +1,5 @@
 
-import { randomInt } from "crypto"
+import fs from "fs"
 
 const testString = "Test string " + randomInt(1000)
 process.stdout.write(testString)
--- 5/ex5/mv.js
+++ 5/ex5/mv.js
@@ -1,4 +1,14 @@
 import fs from "fs"
 
+const data = ""
+const file2 = "./testDir/TestFile2.txt"
+fs.writeFileSync(file2, data, function (err) {
+  if (err) {
+    return console.log(err)
+  }
+  console.log("the file has been created")
+})
 
-fs.renameSync
+const rename = fs.renameSync("./testDir/Testfile.txt", "./testDir/TestFile2.txt")
+const out = fs.readFileSync(rename, "UTF-8")
+console.log(out)
