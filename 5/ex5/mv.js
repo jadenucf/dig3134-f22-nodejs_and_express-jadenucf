f5366b32174f71044549be951ec40f18f9a9083e
Jaden Lewis-Chambers <you@example.com>
2022-10-12 06:13:13 +0000

    Oct 12, 2022, 2:13 AM

--- 5/ex5/mv.js
+++ 5/ex5/mv.js
@@ -1,7 +1,7 @@
 import fs from "fs"
 
 const data = ""
-const file2 = "./testDir/TestFile2.txt"
+const file2 = "./testDir/TestFile3.txt"
 fs.writeFileSync(file2, data, function (err) {
   if (err) {
     return console.log(err)
@@ -9,6 +9,6 @@ fs.writeFileSync(file2, data, function (err) {
   console.log("the file has been created")
 })
 
-const rename = fs.renameSync("./testDir/Testfile.txt", "./testDir/TestFile2.txt")
+const rename = fs.renameSync("./testDir/TestFile.txt", "./testDir/TestFile2.txt")
 const out = fs.readFileSync(rename, "UTF-8")
 console.log(out)
