797fd745bd1179236a4f72464458ede2da442cf9
Jaden Lewis-Chambers <you@example.com>
2022-10-12 06:22:48 +0000

    Oct 12, 2022, 2:22 AM

--- 5/ex5/echo.js
+++ 5/ex5/echo.js
@@ -1,5 +1,5 @@
 
-import rando from "./5/ex5.test.js"
+import rando from "UCF-GaiM/dig3134-f22-nodejs_and_express-jadenucf/5/ex5.test.js"
 
 const testString = "Test string " + rando
 process.stdout.write(testString)
