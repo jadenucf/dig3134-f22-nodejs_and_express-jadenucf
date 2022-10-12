1f8f21fd76698635e83f75e56e7a3f38758bf43a
Jaden Lewis-Chambers <you@example.com>
2022-10-12 12:04:37 -0400

    syncing

--- 5/ex5/echo.js
+++ 5/ex5/echo.js
@@ -4,7 +4,4 @@
 // const __filename = fileURLToPath(import.meta.url)
 // const __dirname = dirname(__filename)
 
-import { hi } from "../ex5.test.js"
-
-console.log(hi)
-process.stdout.write(hi)
+process.stdout.write('1')
