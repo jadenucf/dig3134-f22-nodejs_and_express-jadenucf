bc5f4729658d7685ee3a49242287d886cc4d94b1
Jaden Lewis-Chambers <you@example.com>
2022-10-12 15:15:37 +0000

    Oct 12, 2022, 11:15 AM

--- 5/ex5/echo.js
+++ 5/ex5/echo.js
@@ -1,5 +1,11 @@
 // import { fileURLToPath } from "url"
 // import { dirname } from "path"
 
+
 // const __filename = fileURLToPath(import.meta.url)
 // const __dirname = dirname(__filename)
+
+const gn = require("../ex5.test")
+
+console.log(gn)
+process.stdout.write(gn)
