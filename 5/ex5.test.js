914554a9e66cc945a59fc97af57b8c6538f7e126
Jaden Lewis-Chambers <you@example.com>
2022-10-12 00:48:48 +0000

    Oct 11, 2022, 8:48 PM

--- 5/ex5.test.js
+++ 5/ex5.test.js
@@ -10,7 +10,7 @@ import { fileURLToPath } from "url"
 import { spawn, spawnSync } from "child_process"
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
-export const testString = "f"
+export const testString = "1"
 function cleanTestDir () {
   const files = fs.readdirSync(path.resolve("testDir"))
   for (const file of files) {
