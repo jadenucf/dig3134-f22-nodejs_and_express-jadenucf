cfd9932ea783908496c39930093695f5ec992fe1
Jaden Lewis-Chambers <you@example.com>
2022-10-13 13:13:21 -0400

    sync

---
+++ 5/ex5/mv.js
@@ -0,0 +1,11 @@
+import fs from "fs"
+
+const testFolder = "./testDir"
+
+fs.readdir(testFolder, (err, files) => {
+  if (err) {
+    console.log(err)
+  }
+  console.log(...files)
+  // fs.renameSync(`./testDir/${file}`, "world.txt")
+})
