47982d0b1fa8481b18c88806b30173ec86236e4e
jadenucf <jadenlc@knights.ucf.edu>
2022-10-25 20:49:27 +0000

    Oct 25, 2022, 4:49 PM

--- 5/ex5/index.js
+++ 5/ex5/index.js
@@ -1,5 +1,7 @@
-export function createTimeout () {
+export function createTimeout (seconds, string) {
 // Ex 5-1.1. IMPLEMENT HERE. Add parameters as necessary
+  setTimeout(seconds)
+  console.log(`${string}: ${setTimeout(seconds)}.`)
 }
 export function createInterval () {
 // Ex 5-1.2. IMPLEMENT HERE. Add parameters as necessary
