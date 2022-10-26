558a438f5a4fb2157550cd1c91940e1843db0ce7
Jaden Lewis-Chambers <you@example.com>
2022-10-26 17:48:34 -0400

    commit

--- 6/project2/index.js
+++ 6/project2/index.js
@@ -1,4 +1,26 @@
-const express = require("express")
-const fs = require("fs")
+import express from "express"
+import { data } from "./data/MOCK_DATA.json"
 const app = express()
-const path = require("path")
+
+const PORT = 3000
+
+app.get("/", (request, response) => {
+  response.json(data)
+})
+
+app.post("/create", (request, response) => {
+  response.send("post request at create :3.")
+})
+
+app.put("/edit", (request, response) => {
+  response.send("put request at edit.")
+})
+
+app.delete("/delete", (request, response) => {
+  response.send("delete request at delete.")
+})
+
+app.listen(PORT, () => {
+  console.log(`The server is running on ${PORT}!`)
+  console.log(data)
+})
