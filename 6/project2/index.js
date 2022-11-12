4a345b9aebc89720e1aae0456375479c866a0b75
Jaden Lewis-Chambers <you@example.com>
2022-11-12 18:56:35 -0500

    s

--- 6/project2/index.js
+++ 6/project2/index.js
@@ -1,26 +1,101 @@
 import express from "express"
-import { data } from "./data/MOCK_DATA.json"
+import path from "path"
+import fs from "fs"
+
+import posts from "./data/posts.json" assert {type: 'json'}
 const app = express()
 
 const PORT = 3000
 
-app.get("/", (request, response) => {
-  response.json(data)
+app.use(express.urlencoded({ extended: true }))
+
+
+//GET POSTS
+app.get("/posts", (request, response) => {
+  //sends the entire array
+  response.send(posts)
 })
 
-app.post("/create", (request, response) => {
-  response.send("post request at create :3.")
+//GET ID PARAMETER
+app.get("/posts/:postId", (request, response) => {
+  //finds the specific postid then shares the post
+  const getId = Number(request.params.postId)
+  let id = posts.filter((post) => post.postId === getId)
+
+  response.send(...id)
 })
 
-app.put("/edit", (request, response) => {
-  response.send("put request at edit.")
+//GET AUTHOR PARAMETER
+app.get("/posts/author/:author", (request, response) => {
+  //find all the comments posted by a specific author
+  const getAuthor = request.params.author
+  const author = posts.filter((post) => post.author === getAuthor )
+  response.send(author)
 })
 
-app.delete("/delete", (request, response) => {
-  response.send("delete request at delete.")
+
+
+//POST JSON OBJECT
+app.post("/posts", (request, response) => {
+  //creates a new post and sends it to the main array
+  posts.push(request.body)
+  response.send('sent')
 })
+
+
+
+//DELETE
+app.delete("/delete/:postId", (request, response) => {
+  //deletes a post from the main array
+  const getId = Number(request.params.postId)
+  let index = posts.findIndex((post) => post.postId === getId)
+  posts.splice(index, 1)
+  response.send(posts)
+})
+
+
+
+//POST COMMENT ID
+//creates a comment a person posted within the comment array
+app.post("/posts/:postId/comments/", (request, response) => {
+  const getId = Number(request.params.postId)
+  let id = posts.filter((post) => post.postId === getId)
+
+  const getCommentId = (request.params.postId)
+  id[0].comments.push(request.body)
+  response.send('sent')
+  
+})
+
+
+
+//GET COMMENTS ARRAY FROM SPECIFIC POSTER
+//displays all the comment the poster has posted
+app.get("/posts/:postId/comment/", (request, response) => {
+  const getId = Number(request.params.postId)
+  let id = posts.filter((post) => post.postId === getId)
+  response.send(id[0].comments)
+})
+
+
+
+//DELETES A COMMENT FROM AN OBJECT 
+//deletes a comment from a poster
+app.delete("/posts/:postId/comment/:id", (request, response) => {
+  const getId = Number(request.params.postId)
+  const commentId = request.params.postId
+
+  let id = posts.filter((post) => post.postId === getId)
+
+  let index = id[0].comments.findIndex((comment) => comment.commentId === commentId)
+  id[0].comments.splice(index, 1)
+  response.send(id)
+})
+
+
+// ----------------------------------------------------------------
 
 app.listen(PORT, () => {
   console.log(`The server is running on ${PORT}!`)
-  console.log(data)
+  //console.log(posts)
 })
